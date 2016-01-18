24745fa6897a450d56c37baec322314fa8774182
Mohammed Ersan <mohammed.ersan@mttnow.com>
2016-01-18 16:41:47 +0000

    generate sample

---
+++ sample/src/main/java/com/moshx/gdxr/Assets.java
@@ -0,0 +1,19 @@
+package com.moshx.gdxr;
+
+import java.lang.String;
+
+public class Assets {
+  public static final String CON_IC_SEGMENT_CARD_FLIGHT_PNG = "con_ic_segment_card_flight.png";
+
+  public static final String CON_IC_SEGMENT_CARD_GROUND_PNG = "con_ic_segment_card_ground.png";
+
+  public static final String CON_IC_SEGMENT_CARD_TRAIN_PNG = "con_ic_segment_card_train.png";
+
+  public static class Fonts {
+    public static final String CON_IC_SEGMENT_CARD_FLIGHT_PNG = "fonts/con_ic_segment_card_flight.png";
+
+    public static final String CON_IC_SEGMENT_CARD_GROUND_PNG = "fonts/con_ic_segment_card_ground.png";
+
+    public static final String CON_IC_SEGMENT_CARD_TRAIN_PNG = "fonts/con_ic_segment_card_train.png";
+  }
+}
